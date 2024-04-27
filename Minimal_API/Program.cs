acf0f983194cc67aea08eec9ee438785c1171d75
roboty.p.d <freddik1990@gmail.com>
2024-04-27 11:11:24 +0300

    добавил список отелей

--- Minimal_API/Program.cs
+++ Minimal_API/Program.cs
@@ -2,22 +2,39 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
+var hotels = new List<Hotel>();
 
-app.MapControllers();
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) => {
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0)
+    {
+        throw new Exception("Not found");
+    }
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
+
 
 app.Run();
+
+
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
